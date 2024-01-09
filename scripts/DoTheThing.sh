211b8bcd4aac3b8505cebf75b95ea16d708f1d3e
Matt Ruiz <matt.ruiz@mesh-systems.com>
2024-01-09 15:40:54 -0500

    added script to run create and upload certs all at once

---
+++ scripts/DoTheThing.sh
@@ -0,0 +1,19 @@
+./certificateAuthority.sh
+
+./adminClientCertificate.sh
+
+./kubeletClientCertificate.sh
+
+./controllerManagerClientCertificate.sh
+
+./kubeProxyClientCertificate.sh
+
+./schedulerClientCertificate.sh
+
+./kubernetesAPIServerCert.sh
+
+./serviceAccountKeyPair.sh
+
+./distributeKeys.sh
+
+rm -f *.csr *.json *.pem
